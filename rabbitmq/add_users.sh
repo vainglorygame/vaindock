5b44df19c674c20e78747e8fbe035b421c9b369a
schneefux <schneefux+commit@schneefux.xyz>
2017-04-03 16:26:41 +0200

    rename updater -> bridge

--- rabbitmq/add_users.sh
+++ rabbitmq/add_users.sh
@@ -5,15 +5,15 @@ set -e
 count=0;
 # Execute list_users until service is up and running
 until timeout 5 rabbitmqctl list_users >/dev/null 2>/dev/null || (( count++ >= 20 )); do sleep 1; done;
-if ! rabbitmqctl list_users | grep updater > /dev/null
+if ! rabbitmqctl list_users | grep bridge > /dev/null
 then
    # rabbitmqctl delete_user guest
    rabbitmqctl add_user web web
    rabbitmqctl set_permissions -p / web "^stomp-subscription-" "^stomp-subscription-" "^(amq.topic$|stomp-subscription-)"
    # read only amq.topic on host /, but allow stomp subscribe
    # services below: all allowed
-   rabbitmqctl add_user updater updater
-   rabbitmqctl set_permissions -p / updater ".*" ".*" ".*"
+   rabbitmqctl add_user bridge bridge
+   rabbitmqctl set_permissions -p / bridge ".*" ".*" ".*"
    rabbitmqctl add_user apigrabber apigrabber
    rabbitmqctl set_permissions -p / apigrabber ".*" ".*" ".*"
    rabbitmqctl add_user processor processor
