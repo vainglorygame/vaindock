dae862df22cec811513e067969bcbea5d4551d75
schneefux <schneefux+commit@schneefux.xyz>
2017-07-14 19:04:02 +0200

    2.14.0 rewrite

--- rabbitmq/add_users.sh
+++ rabbitmq/add_users.sh
@@ -17,10 +17,12 @@ rabbitmqctl add_user bridge bridge
 rabbitmqctl set_permissions -p / bridge ".*" ".*" ".*"
 rabbitmqctl add_user apigrabber apigrabber
 rabbitmqctl set_permissions -p / apigrabber ".*" ".*" ".*"
-rabbitmqctl add_user sampler sampler
-rabbitmqctl set_permissions -p / sampler ".*" ".*" ".*"
+rabbitmqctl add_user telesucker telesucker
+rabbitmqctl set_permissions -p / telesucker ".*" ".*" ".*"
 rabbitmqctl add_user processor processor
 rabbitmqctl set_permissions -p / processor ".*" ".*" ".*"
+rabbitmqctl add_user shrinker shrinker
+rabbitmqctl set_permissions -p / shrinker ".*" ".*" ".*"
 rabbitmqctl add_user analyzer analyzer
 rabbitmqctl set_permissions -p / analyzer ".*" ".*" ".*"
 rabbitmqctl add_user cruncher cruncher
